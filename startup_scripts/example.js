25151d61da81c8924fb7bb00cd32dc8c345c3492
mr-sodium <jk8467445@gmail.com>
2025-12-08 17:20:28 -0500

    added the code base
    read the title

---
+++ startup_scripts/example.js
@@ -0,0 +1,9 @@
+StartupEvents.registry('item', event => {
+    event.create('blazing_upgrade_template').displayName('blazing upgrade template')
+})
+StartupEvents.registry('item', event => {
+    event.create('gilded_upgrade_template').displayName('guilded upgrade template')
+})
+StartupEvents.registry('item', event => {
+    event.create('rose_quartz_dust').displayName('rose qu   artz dust')
+})
