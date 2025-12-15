d3f134a4adaa63753f5be435640c59b628b7e6e6
bbitsme <hascallblake71@gmail.com>
2025-12-14 21:20:09 -0500

    teehee

--- startup_scripts/loadup.js
+++ startup_scripts/loadup.js
@@ -1,9 +1,12 @@
 StartupEvents.registry('item', event => {
-    event.create('blazing_upgrade_template').displayName('blazing upgrade template')
+    event.create('blazing_upgrade_template').displayName('Blazing upgrade template')
 })
 StartupEvents.registry('item', event => {
-    event.create('gilded_upgrade_template').displayName('guilded upgrade template')
+    event.create('gilded_upgrade_template').displayName('Guilded upgrade template')
 })
 StartupEvents.registry('item', event => {
-    event.create('rose_quartz_dust').displayName('rose quartz dust')
+    event.create('rose_quartz_dust').displayName('Rose quartz dust')
+})
+StartupEvents.registry('item', event => {
+    event.create('carbon_dust').displayName('Carbon dust')
 })
