3874950d2190859c4d16bff86404f439d3ae83a2
bbitsme <hascallblake71@gmail.com>
2025-12-16 21:16:33 -0500

    yup

--- startup_scripts/loadup.js
+++ startup_scripts/loadup.js
@@ -1,13 +1,7 @@
 StartupEvents.registry('item', event => {
     event.create('blazing_upgrade_template').displayName('Blazing upgrade template')
-})
-StartupEvents.registry('item', event => {
     event.create('gilded_upgrade_template').displayName('Guilded upgrade template')
-})
-StartupEvents.registry('item', event => {
     event.create('rose_quartz_dust').displayName('Rose quartz dust')
-})
-StartupEvents.registry('item', event => {
     event.create('carbon_dust').displayName('Carbon dust')
 })
 //potato
