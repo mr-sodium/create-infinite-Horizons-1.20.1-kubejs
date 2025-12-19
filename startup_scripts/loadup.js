79e1a8ee79f4714805a0b66ec306d5a3e97cc243
bbitsme <hascallblake71@gmail.com>
2025-12-19 16:21:42 -0500

    added and changed stuff

--- startup_scripts/loadup.js
+++ startup_scripts/loadup.js
@@ -3,5 +3,6 @@ StartupEvents.registry('item', event => {
     event.create('gilded_upgrade_template').displayName('Guilded upgrade template')
     event.create('rose_quartz_dust').displayName('Rose quartz dust')
     event.create('carbon_dust').displayName('Carbon dust')
+    event.create('brass_pipe').displayName('Brass pipe')
 })
 //potato
