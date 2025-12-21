c2f947ceda2870ae42159947fbefc8b1cbeb8410
bbitsme <hascallblake71@gmail.com>
2025-12-21 01:37:31 -0500

    .

--- server_scripts/fire_item.js
+++ server_scripts/fire_item.js
@@ -1,9 +1,15 @@
 PlayerEvents.tick(event => {
     const {player} = event
-    const fire_item = 'kubejs:blazing_upgrade_template'
+    const burn_baby_burn = 'kubejs:blazing_upgrade_template'
+    const lava_burn = 'minecraft:lava_bucket'
 
     if (player.level.isClientSide()) return
-        if (player.mainHandItem.id == fire_item || player.offHandItem.id == fire_item) {
+        if (player.mainHandItem.id == burn_baby_burn || player.offHandItem.id == burn_baby_burn) {
         player.setSecondsOnFire(2)
     }
+
+    if (player.level.isClientSide()) return
+        if (player.mainHandItem.id == lava_burn || player.offHandItem.id == lava_burn) {
+        player.setSecondsOnFire(3)
+    }    
 })
