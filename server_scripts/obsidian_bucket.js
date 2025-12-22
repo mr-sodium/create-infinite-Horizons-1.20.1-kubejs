794a68ba07c7935f0e7c7a62553a957286c45a14
bbitsme <hascallblake71@gmail.com>
2025-12-21 20:20:28 -0500

    obsidian buckets

--- server_scripts/obsidian_bucket.js
+++ server_scripts/obsidian_bucket.js
@@ -1,10 +1,45 @@
-BlockEvents.rightClicked(event => {
-  const bucket = 'kubejs:obsidian_bucket'
-  const lava_bucket = 'kubejs:obsidian_lava_bucket'
-  const {player} = event
+ItemEvents.rightClicked('kubejs:obsidian_bucket', event => {
+   const {player, item, target} = event
+    
+   if (target && target.block && target.block.id == 'minecraft:lava' && target.block.properties.level == 0) {
+      target.block.set('minecraft:air')
+      item.count--
+      player.give('kubejs:obsidian_lava_bucket')
+      event.cancel()
+   }
+})
 
-   if (player.mainHandItem.id == bucket) {
-      event.item.count--
-      player.giveInHand(lava_bucket)
+ItemEvents.rightClicked('kubejs:obsidian_lava_bucket', event => {
+   const {player, item, target, level} = event
+
+   if (target.type == 'BLOCK') {
+      let placePos = target.block.pos.relative(target.facing)
+      level.getBlock(placePos).set('minecraft:lava')
+      item.count--
+      player.give('kubejs:obsidian_bucket')
+      event.cancel()
+   }
+})
+
+ItemEvents.rightClicked('kubejs:obsidian_bucket', event => {
+   const {player, item, target} = event
+    
+   if (target && target.block && target.block.id == 'minecraft:water' && target.block.properties.level == 0) {
+      target.block.set('minecraft:air')
+      item.count--
+      player.give('kubejs:obsidian_water_bucket')
+      event.cancel()
+   }
+})
+
+ItemEvents.rightClicked('kubejs:obsidian_water_bucket', event => {
+   const {player, item, target, level} = event
+
+   if (target.type == 'BLOCK') {
+      let placePos = target.block.pos.relative(target.facing)
+      level.getBlock(placePos).set('minecraft:water')
+      item.count--
+      player.give('kubejs:obsidian_bucket')
+      event.cancel()
    }
 })
--- startup_scripts/items.js
+++ startup_scripts/items.js
@@ -1,10 +1,12 @@
 //potato
 StartupEvents.registry('item', event => {
-    event.create('blazing_upgrade_template').displayName('Blazing upgrade template')
-    event.create('gilded_upgrade_template').displayName('Guilded upgrade template')
-    event.create('rose_quartz_dust').displayName('Rose quartz dust')
-    event.create('carbon_dust').displayName('Carbon dust')
-    event.create('brass_pipe').displayName('Brass pipe')
-    event.create('obsidian_bucket').displayName('Obsidian bucket').maxStackSize(16)
-    event.create('obsidian_lava_bucket').displayName('Obsidian lava bucket').maxStackSize(1)
+    event.create('blazing_upgrade_template').displayName('Blazing Upgrade Template')
+    event.create('gilded_upgrade_template').displayName('Guilded Upgrade Template')
+    event.create('rose_quartz_dust').displayName('Rose Quartz Dust')
+    event.create('carbon_dust').displayName('Carbon Dust')
+    event.create('brass_pipe').displayName('Brass Pipe')
+    event.create('obsidian_bucket').displayName('Obsidian Bucket').maxStackSize(16)
+    event.create('obsidian_lava_bucket').displayName('Obsidian Lava Bucket').maxStackSize(1)
+    event.create('obsidian_water_bucket').displayName('Obsidian Water Bucket').maxStackSize(1)
+  
 })
