2baf654c0324455c8da974349bd7b45a466bf7c9
bbitsme <hascallblake71@gmail.com>
2025-12-22 13:03:45 -0500

    .

--- server_scripts/obsidian_buckets.js
+++ server_scripts/obsidian_buckets.js
@@ -8,6 +8,20 @@ ItemEvents.rightClicked('kubejs:obsidian_bucket', event => {
       player.swing()
       event.cancel()
    }
+   if (target && target.block && target.block.id == 'minecraft:water' && target.block.properties.level == 0) {
+      target.block.set('minecraft:')
+      item.count--
+      player.give('kubejs:obsidian_water_bucket')
+      player.swing()
+      event.cancel()
+   }
+   if (target && target.block && target.block.id == 'minecraft:powder_snow') {
+      target.block.set('minecraft:air')
+      item.count--
+      player.give('kubejs:obsidian_powder_snow_bucket')
+      player.swing()
+      event.cancel()
+   }
 })
 
 ItemEvents.rightClicked('kubejs:obsidian_lava_bucket', event => {
@@ -45,18 +59,6 @@ ItemEvents.rightClicked('kubejs:obsidian_lava_bucket', event => {
 
 })
 
-ItemEvents.rightClicked('kubejs:obsidian_bucket', event => {
-   const {player, item, target} = event
-    
-   if (target && target.block && target.block.id == 'minecraft:water' && target.block.properties.level == 0) {
-      target.block.set('minecraft:')
-      item.count--
-      player.give('kubejs:obsidian_water_bucket')
-      player.swing()
-      event.cancel()
-   }
-})
-
 ItemEvents.rightClicked('kubejs:obsidian_water_bucket', event => {
    const {player, item, target, level} = event
 
@@ -83,6 +85,29 @@ ItemEvents.rightClicked('kubejs:obsidian_water_bucket', event => {
       if (target.type == 'BLOCK' && target.block.id != 'minecraft:water') {
          let placePos = target.block.pos.relative(target.facing)
          level.getBlock(placePos).set('minecraft:water')
+         item.count--
+         player.give('kubejs:obsidian_bucket')
+      }
+      player.swing()
+      event.cancel()
+   }
+})
+
+ItemEvents.rightClicked('kubejs:obsidian_powder_snow_bucket', event => {
+   const {player, item, target, level} = event
+
+   if (player.creative) {
+      if (target.type == 'BLOCK') {
+         let placePos = target.block.pos.relative(target.facing)
+         level.getBlock(placePos).set('minecraft:powder_snow')
+      }
+      player.swing()
+      event.cancel()
+   }
+   if (!player.creative) {
+      if (target.type == 'BLOCK') {
+         let placePos = target.block.pos.relative(target.facing)
+         level.getBlock(placePos).set('minecraft:powder_snow')
          item.count--
          player.give('kubejs:obsidian_bucket')
       }
