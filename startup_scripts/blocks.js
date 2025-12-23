7787adeeb30b9265f9fb4557a7ea3f72294e7bc9
mr-sodium <jk8467445@gmail.com>
2025-12-23 17:22:33 -0500

    Update blocks.js
    ???

--- startup_scripts/blocks.js
+++ startup_scripts/blocks.js
@@ -1,4 +1,3 @@
 StartupEvents.registry('block', event => {
-  event.create('charcoal_block').displayName('Block of Charcoal').soundType('stone').hardness(5).resistance(
-6).tagBlock('minecraft:mineable/pickaxe')
+  event.create('charcoal_block').displayName('Block of Charcoal').soundType('stone').hardness(5).resistance(6).tagBlock('minecraft:mineable/pickaxe')
 })
