d595ab071dd5f2d3d8025d09040acec304f48319
bbitsme <hascallblake71@gmail.com>
2025-12-31 21:05:45 -0500

    .

--- server_scripts/fire_item.js
+++ server_scripts/fire_item.js
@@ -4,12 +4,14 @@ PlayerEvents.tick(event => {
     const lava_burn = 'minecraft:lava_bucket'
 
     if (player.level.isClientSide()) return
-        if (player.mainHandItem.id == burn_baby_burn || player.offHandItem.id == burn_baby_burn) {
+
+    if (player.mainHandItem.id == burn_baby_burn || player.offHandItem.id == burn_baby_burn) {
         player.setSecondsOnFire(2)
     }
-
-    if (player.level.isClientSide()) return
+    if (!player.creative) {
         if (player.mainHandItem.id == lava_burn || player.offHandItem.id == lava_burn) {
         player.setSecondsOnFire(3)
+        player.getSlot(player.selectedSlot).set('minecraft:air')
+        }
     }    
 })
