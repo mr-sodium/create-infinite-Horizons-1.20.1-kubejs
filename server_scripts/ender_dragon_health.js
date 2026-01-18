276b9364b0f08b46e6dd6e49903e94a2014d73fe
mr-sodium <jk8467445@gmail.com>
2026-01-17 21:21:28 -0500

    a
    a

---
+++ server_scripts/ender_dragon_health.js
@@ -0,0 +1,21 @@
+// KubeJS server script to set Ender Dragon max health to 1000
+
+EntityEvents.spawned(event => {
+  const entity = event.entity
+  const Dhealth = 200
+
+  // Check if the spawned entity is the Ender Dragon
+  if (entity.type == "minecraft:ender_dragon") {
+    console.log("ENDERDRAGON SPAWNED")
+
+    // Get the generic max health attribute
+    const maxHealthAttr = entity.getAttribute("minecraft:generic.max_health")
+
+    if (maxHealthAttr) {
+      maxHealthAttr.baseValue = Dhealth
+
+      // Also heal the dragon to full after changing health
+      entity.health = Dhealth
+    }
+   }
+ })
