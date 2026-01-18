f72717507d61696c346f723d9a6a5b52dc071c2c
bbitsme <hascallblake71@gmail.com>
2026-01-17 21:12:28 -0500

    .

--- server_scripts/better obsidian buckets.js
+++ server_scripts/better obsidian buckets.js
@@ -34,8 +34,8 @@ ItemEvents.rightClicked(event => {
     if (fluidToPlace) {
         const placePos = target.block.offset(target.facing)
         const targetBlock = level.getBlock(placePos)
-
-        if (targetBlock.id == 'minecraft:air' || targetBlock.canBeReplaced()) {
+        
+        if (targetBlock.id == 'minecraft:air' && target.block.properties.level != 0 || targetBlock.canBeReplaced()) {
             targetBlock.set(fluidToPlace)
             player.swing()
             event.cancel()
