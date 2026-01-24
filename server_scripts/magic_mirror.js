60f348166fc9fe5d5213d2879e8597d20b9e8976
mr-sodium <jk8467445@gmail.com>
2026-01-24 01:21:18 -0500

    yourmom.fm

--- server_scripts/magic_mirror.js
+++ server_scripts/magic_mirror.js
@@ -3,19 +3,25 @@ ItemEvents.rightClicked(event => {
   const item = event.item
   const spawnPos = player.getRespawnPosition()
 
-  if (item.id !== "kubejs:magic_mirror") return
+  if (item.id !== "kubejs:magic_mirror") {return} 
+  event.server.runCommand(`/playsound block.beacon.deactivate master @p ${player.x} ${player.y} ${player.z} 2 0.9`);
+  console.log(player.level.id) // cannot parse dimention rn FIX
+  if (player.dimension.id === 'minecraft:overworld'){
 
-  if (!spawnPos) {
-    let newspawnPos = player.level.getSharedSpawnPos()
-    player.teleportTo(
-    newspawnPos.x + 0.5,
-    newspawnPos.y + 0.6,
-    newspawnPos.z + 0.5
-  )
+    if (!spawnPos) {
+      let newspawnPos = player.level.getSharedSpawnPos()
+      player.teleportTo(
+      newspawnPos.x + 0.5,
+      newspawnPos.y + 0.6,
+      newspawnPos.z + 0.5
+    )}else{
+      player.teleportTo(
+      spawnPos.x + 0.5,
+      spawnPos.y + 0.6,
+      spawnPos.z + 0.5
+    )}
   }else{
-    player.teleportTo(
-    spawnPos.x + 0.5,
-    spawnPos.y + 0.6,
-    spawnPos.z + 0.5
-  )}
+    event.server.runCommand(`/playsound block.beacon.deactivate master @p ${player.x} ${player.y} ${player.z} 2 0.9`);
+
+  }
 })
