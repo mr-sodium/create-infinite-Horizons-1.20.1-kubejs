5431266e4ec748a66359218889c43bc3988bf615
mr-sodium <jk8467445@gmail.com>
2026-01-24 14:59:11 -0500

    Update magic_mirror.js

--- server_scripts/magic_mirror.js
+++ server_scripts/magic_mirror.js
@@ -3,10 +3,17 @@ ItemEvents.rightClicked(event => {
   const item = event.item
   const spawnPos = player.getRespawnPosition()
 
+  const dimension = player.level.dimension.toString();
+
+
+  //blake this checks if you hold thing
   if (item.id !== "kubejs:magic_mirror") {return} 
-  event.server.runCommand(`/playsound block.beacon.deactivate master @p ${player.x} ${player.y} ${player.z} 2 0.9`);
-  console.log(player.level.id) // cannot parse dimention rn FIX
-  if (player.dimension.id === 'minecraft:overworld'){
+  
+  // for debug
+  console.log(dimension) 
+  
+  // do not delete, this if for the future
+  if (dimension == "minecraft:overworld" && player.hasCooldown(item.id)){
 
     if (!spawnPos) {
       let newspawnPos = player.level.getSharedSpawnPos()
@@ -20,6 +27,7 @@ ItemEvents.rightClicked(event => {
       spawnPos.y + 0.6,
       spawnPos.z + 0.5
     )}
+    // put damage item code here
   }else{
     event.server.runCommand(`/playsound block.beacon.deactivate master @p ${player.x} ${player.y} ${player.z} 2 0.9`);
 
