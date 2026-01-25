83c3d6a3538f9b2f5f103718bf88682a9f371954
bbitsme <hascallblake71@gmail.com>
2026-01-25 18:00:31 -0500

    Update magic_mirror.js

--- server_scripts/magic_mirror.js
+++ server_scripts/magic_mirror.js
@@ -2,7 +2,7 @@ ItemEvents.rightClicked(event => {
   const {player, item, hand} = event
   const spawnPos = player.getRespawnPosition()
   const dimension = player.level.dimension.toString()
-  
+
   if (item.id !== "kubejs:magic_mirror" && item.id !== "kubejs:damaged_magic_mirror" && item.id !== "kubejs:broken_magic_mirror" && item.id !== "kubejs:cracked_magic_mirror") {
     return
   }
@@ -20,7 +20,7 @@ ItemEvents.rightClicked(event => {
       spawnPos.y + 0.5625,
       spawnPos.z + 0.5
     )}
-//
+
     event.server.runCommand(`/playsound minecraft:block.amethyst_block.step master @p ${player.x} ${player.y} ${player.z} 1 1.3`)
     event.server.runCommand(`/playsound minecraft:block.bone_block.place master @p ${player.x} ${player.y} ${player.z} 3 2`)
     event.server.runCommand(`/playsound minecraft:block.amethyst_block.break master @p ${player.x} ${player.y} ${player.z} 1 0.2`)
