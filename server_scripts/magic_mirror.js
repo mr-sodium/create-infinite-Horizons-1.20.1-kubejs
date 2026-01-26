81cf8ced32e3ce763608a1fd41ca951ee016a9d7
mr-sodium <jk8467445@gmail.com>
2026-01-25 20:24:36 -0500

    Update magic_mirror.js
    made cooldown universal

--- server_scripts/magic_mirror.js
+++ server_scripts/magic_mirror.js
@@ -3,6 +3,7 @@ ItemEvents.rightClicked(event => {
   const spawnPos = player.getRespawnPosition()
   const respawnDim = player.getRespawnDimension()
   const dimension = player.level.dimension.toString()
+  const cooldowntime = 600;
   const mirrors = [
   "kubejs:magic_mirror",
   "kubejs:cracked_magic_mirror",
@@ -18,14 +19,14 @@ ItemEvents.rightClicked(event => {
 
   event.server.runCommand(`/gamerule sendCommandFeedback false`)
 
-  player.addItemCooldown(item.id, 600)
+  
   
   if (dimension == "minecraft:overworld" && item.id !== "kubejs:broken_magic_mirror"){
     if (!spawnPos) {
       let newspawnPos = player.level.getSharedSpawnPos()
       player.teleportTo(
       newspawnPos.x + 0.5,
-      newspawnPos.y + 0.6,
+      newspawnPos.y + 0.5,
       newspawnPos.z + 0.5
     )}else{
       player.teleportTo(
@@ -49,7 +50,12 @@ ItemEvents.rightClicked(event => {
         player.setItemInHand(hand, Item.of("kubejs:broken_magic_mirror"))
         event.server.runCommand(`/playsound minecraft:block.glass.break master @p ${player.x} ${player.y} ${player.z} 1.5 0.7`)
       }}
+    player.addItemCooldown("kubejs:magic_mirror", cooldowntime)
+    player.addItemCooldown("kubejs:cracked_magic_mirror", cooldowntime)
+    player.addItemCooldown("kubejs:damaged_magic_mirror", cooldowntime)
+    
   }else{
     event.server.runCommand(`/playsound block.beacon.deactivate master @p ${player.x} ${player.y} ${player.z} 2 0.9`)
+    player.addItemCooldown("kubejs:broken_magic_mirror", cooldowntime)
   }
 })
