53eace6db9cf4eb9765ebf504405ef8697674da9
mr-sodium <jk8467445@gmail.com>
2026-01-25 19:45:21 -0500

    a
    a

--- client_scripts/capes.js
+++ client_scripts/capes.js
@@ -1,3 +1,5 @@
-CapeJS.addCapes(event => {
-    event.register("cda8bd02-a97d-4d2d-a391-32f4a0161c43", "template");//halp
+CapeJS.addCapes(event => {// to add people just copy the event.regitster line and add a .png in the assets/capejs/textures/capes
+    event.register("cda8bd02-a97d-4d2d-a391-32f4a0161c43", "bbitsme1");//mr__sodium
+    event.register("22648a62-1c2c-4a5d-abd8-1f710245e630", "you_moms_79");//you_mom_79
+    event.register("a3e662c0-f410-4df7-9621-2fbffa316d70", "bbitsme1");//bbitsme1
 });
