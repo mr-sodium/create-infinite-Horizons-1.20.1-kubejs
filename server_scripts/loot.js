e4bef84c1ddbf937faa6d36a5496b74ef581189a
bbitsme <hascallblake71@gmail.com>
2026-02-01 15:12:33 -0500

    Update loot.js

--- server_scripts/loot.js
+++ server_scripts/loot.js
@@ -2,5 +2,4 @@ LootJS.modifiers((event) => {
     //this need you to have loot.js DO NOT DELETE
     event.addBlockLootModifier("kubejs:cracked_diamond_block").removeLoot("kubejs:cracked_diamond_block").addLoot("minecraft:diamond_block")
     event.addBlockLootModifier("kubejs:budding_durrite").removeLoot("kubejs:budding_durrite").addLoot("dungeonnowloading:durite")
-
-});
+})
