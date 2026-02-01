5cbe8eee84ee559bdbc6e5c10dcb1596d77c138e
bbitsme <hascallblake71@gmail.com>
2026-02-01 15:12:11 -0500

    Deleted files that have nothing to do with the durite

--- server_scripts/rccitem.js
+++
@@ -1,7 +0,0 @@
-ItemEvents.rightClicked(event => {
-  const item = event.item
-
-  if (item.hasTag('minecraft:c_rccitems')) {
-    event.server.runCommand('gamerule sendCommandFeedback false')
-  }
-})
