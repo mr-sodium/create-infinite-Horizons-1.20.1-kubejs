f660f23a9540e5911873c54619dd131fabc870cf
mr-sodium <jk8467445@gmail.com>
2026-02-01 13:49:15 -0500

    Update blocks.js

--- startup_scripts/blocks.js
+++ startup_scripts/blocks.js
@@ -1,6 +1,6 @@
 StartupEvents.registry('block', event => {
   event.create('charcoal_block').displayName('Block of Charcoal').soundType('stone').hardness(5).resistance(6).tagBlock('minecraft:mineable/pickaxe')
-  event.create('budding_durrite').displayName('budding durrite').soundType('amethyst').tagBlock('minecraft:mineable/pickaxe').randomTick()
+  event.create('budding_durrite').displayName('budding durrite').soundType('amethyst').tagBlock('minecraft:mineable/pickaxe')
   event.create('cracked_diamond_block').displayName('cracked diamond block').soundType('stone').tagBlock('minecraft:mineable/pickaxe').hardness(5).resistance(6)
   event.create('cracked_diamond_block_l1').displayName('cracked diamond block').soundType('stone').tagBlock('minecraft:mineable/pickaxe').hardness(5).resistance(6)
 })
