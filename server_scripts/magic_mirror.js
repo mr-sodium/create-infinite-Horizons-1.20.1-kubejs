5027aef73dec57cf0bc8393bc3b7b699ff438321
bbitsme <hascallblake71@gmail.com>
2026-03-13 14:37:09 -0400

    .

--- server_scripts/magic_mirror.js
+++ server_scripts/magic_mirror.js
@@ -46,10 +46,11 @@ ItemEvents.rightClicked(event => {
         player.setItemInHand(hand, Item.of("kubejs:broken_magic_mirror"))
         event.server.runCommand(`/playsound minecraft:block.glass.break block @p ${player.x} ${player.y} ${player.z} 1.5 0.7`)
       }
+      player.addItemCooldown("kubejs:magic_mirror", cooldowntime)
+      player.addItemCooldown("kubejs:cracked_magic_mirror", cooldowntime)
+      player.addItemCooldown("kubejs:damaged_magic_mirror", cooldowntime)
     }
-    player.addItemCooldown("kubejs:magic_mirror", cooldowntime)
-    player.addItemCooldown("kubejs:cracked_magic_mirror", cooldowntime)
-    player.addItemCooldown("kubejs:damaged_magic_mirror", cooldowntime)
+    
     
   }else{
     event.server.runCommandSilent(`/playsound block.beacon.deactivate block @p ${player.x} ${player.y} ${player.z} 2 0.9`)  }
