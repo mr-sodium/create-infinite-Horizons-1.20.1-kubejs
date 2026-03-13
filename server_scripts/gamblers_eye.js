73f06a6fda3e39a4c6f88b2dbdbbe2c9d027b4ad
bbitsme <hascallblake71@gmail.com>
2026-03-13 19:49:25 -0400

    Update gamblers_eye.js

--- server_scripts/gamblers_eye.js
+++ server_scripts/gamblers_eye.js
@@ -15,44 +15,31 @@ ItemEvents.rightClicked('kubejs:gamblers_eye', event => {
     
     if(result == 1){
         player.give(Item.of('endrem:exotic_eye'))
-    }
-    if(result == 2){
+    } else if(result == 2){
         player.give(Item.of('endrem:cryptic_eye'))
-    }
-    if(result == 3){
+    } else if(result == 3){
         player.give(Item.of('endrem:guardian_eye'))
-    }
-    if(result == 4){
+    } else if(result == 4){
         player.give(Item.of('endrem:wither_eye'))
-    }
-    if(result == 5){
+    } else if(result == 5){
         player.give(Item.of('endrem:evil_eye'))
-    }
-    if(result == 6){
+    } else if(result == 6){
         player.give(Item.of('endrem:corrupted_eye'))
-    }
-    if(result == 9){
+    } else if(result == 9){
         player.give(Item.of('endrem:old_eye'))
-    }
-    if(result == 8){
+    } else if(result == 8){
         player.give(Item.of('endrem:witch_eye'))
-    }
-    if(result == 7){
+    } else if(result == 7){
         player.give(Item.of('endrem:magical_eye'))
-    }
-    if(result == 10){
+    } else if(result == 10){
         player.give(Item.of('endrem:nether_eye'))
-    }
-    if(result == 11){
+    } else if(result == 11){
         player.give(Item.of('endrem:undead_eye'))
-    }
-    if(result == 12){
+    } else if(result == 12){
         player.give(Item.of('endrem:cold_eye'))
-    }
-    if(result == 13){
+    } else if(result == 13){
         player.give(Item.of('minecraft:ender_eye'))
-    }
-    if(result == 14){
+    } else if(result == 14){
         if(r2 == 1){
             player.give(Item.of('minecraft:ender_eye'))
         }else{
