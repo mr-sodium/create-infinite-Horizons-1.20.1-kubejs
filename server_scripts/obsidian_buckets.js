100354aa6d736ca49a9b93bbb4d657c9c55329ec
bbitsme <hascallblake71@gmail.com>
2026-03-14 22:28:58 -0400

    Update obsidian_buckets.js

--- server_scripts/obsidian_buckets.js
+++ server_scripts/obsidian_buckets.js
@@ -1,87 +1,3 @@
-//ItemEvents.rightClicked('kubejs:obsidian_bucket', event => {
-//    const {item, target, player} = event
-//    const targetBlock = target.block
-//    const targetBlockId = targetBlock.id
-//    const fillMap = {
-//        'minecraft:water': 'kubejs:obsidian_water_bucket',
-//        'minecraft:lava': 'kubejs:obsidian_lava_bucket',
-//        'minecraft:powder_snow': 'kubejs:obsidian_powder_snow_bucket'
-//    }
-//
-//    if (!player.creative) {
-//        item.shrink(1)
-//    }
-//    if (fillMap[targetBlockId] && (targetBlock.properties.level == 0 || target.block.id == 'minecraft:powder_snow')) {
-//        targetBlock.set('minecraft:air')
-//        const filledBucketId = fillMap[targetBlockId]
-//        player.give(filledBucketId)
-//        player.swing()
-//        if (targetBlockId == 'minecraft:water') {
-//            event.server.runCommandSilent(`/playsound minecraft:item.bucket.fill master @p ${player.x} ${player.y} ${player.z} 1 1`)
-//        }
-//        if (targetBlockId == 'minecraft:lava') {
-//            event.server.runCommandSilent(`/playsound minecraft:item.bucket.fill_lava master @p ${player.x} ${player.y} ${player.z} 1 1`)
-//        }
-//        if (targetBlockId == 'minecraft:powder_snow') {
-//            event.server.runCommandSilent(`/playsound minecraft:item.bucket.fill_powder_snow master @p ${player.x} ${player.y} ${player.z} 1 1`)
-//        }
-//        event.cancel()
-//    }
-//})
-//
-// ItemEvents.rightClicked(event => {
-//    const { item, target, player, level } = event
-//    const placeMap = {
-//        'kubejs:obsidian_water_bucket': 'minecraft:water',
-//        'kubejs:obsidian_lava_bucket': 'minecraft:lava',
-//        'kubejs:obsidian_powder_snow_bucket': 'minecraft:powder_snow'
-//    }
-//    const fluidToPlace = placeMap[item.id]
-
-//    if (!fluidToPlace) return
-
-//    let placed = false 
-//    if (target.block.id == fluidToPlace && target.block.properties.level != 0) {
-//        target.block.set(fluidToPlace)
-//        player.swing()
-//        placed = true
-//    } else {
-//        const placePos = target.block.offset(target.facing)
-//        const targetBlock = level.getBlock(placePos)
-//        const belowBlock = level.getBlock(placePos.offset(0, -1, 0))
-
-//        if (belowBlock && belowBlock.id == fluidToPlace && target.block.id != 'minecraft:powder_snow') {
-//            return
-//        }
-
-//        if (targetBlock.id == 'minecraft:air' || targetBlock.canBeReplaced()) {
-//            targetBlock.set(fluidToPlace)
-//            player.swing()
-//            placed = true
-//        }
-//    }
-
-//    if (placed) {
-//        if (fluidToPlace == 'minecraft:water') {
-//            event.server.runCommandSilent(`/playsound minecraft:item.bucket.empty block @p ${player.x} ${player.y} ${player.z} 1 1`)
-//        }
-//        if (fluidToPlace == 'minecraft:lava') {
-//            event.server.runCommandSilent(`/playsound minecraft:item.bucket.empty_lava block @p ${player.x} ${player.y} ${player.z} 1 1`)
-//        }
-//        if (fluidToPlace == 'minecraft:powder_snow') {
-//            event.server.runCommandSilent(`/playsound minecraft:item.bucket.empty_powder_snow block @p ${player.x} ${player.y} ${player.z} 1 1`)
-//        }
-       
-//        if (!player.creative) {
-//            item.shrink(1)
-//            player.give('kubejs:obsidian_bucket')
-//        }
-//        event.cancel()
-//    }
-// })
-
-
-
 ItemEvents.entityInteracted('kubejs:obsidian_bucket', event => {
    const {item, player, target} = event
 
@@ -91,17 +7,9 @@ ItemEvents.entityInteracted('kubejs:obsidian_bucket', event => {
    target.playSound('minecraft:entity.cow.milk')
 })
 
-
-
-
-
-
-
 ItemEvents.rightClicked(event => {
     const { player, item, level, target } = event
-    const placeReach = 4.5
-    const pickUpReach = 4.5
-
+    const reach = 4.5
     const dimension = player.level.dimension.toString()
 
     if (dimension == "minecraft:the_nether" && item.id == "kubejs:obsidian_water_bucket") return
@@ -115,7 +23,7 @@ ItemEvents.rightClicked(event => {
     const fluidToPlace = placeMap[item.id]
     if (!fluidToPlace) return
 
-    const result = player.rayTrace(placeReach, true)
+    const result = player.rayTrace(reach, true)
     if (!result || !result.block) return
 
     let placed = false
@@ -153,6 +61,44 @@ ItemEvents.rightClicked(event => {
         if (!player.creative) {
             item.shrink(1)
             player.give('kubejs:obsidian_bucket')
+        }
+    }
+})
+
+ItemEvents.rightClicked(event => {
+    const { player, item } = event
+    const reach = 4.5
+
+    if (item.id !== 'kubejs:obsidian_bucket') return
+
+    const result = player.rayTrace(reach, true)
+    if (!result || !result.block) return
+
+    const block = result.block
+    let pickedUpItem = null
+    let sound = ''
+
+    if (block.id == 'minecraft:water' && block.properties.level == 0) {
+        pickedUpItem = 'kubejs:obsidian_water_bucket'
+        sound = 'minecraft:item.bucket.fill'
+    } else if (block.id == 'minecraft:lava' && block.properties.level == 0) {
+        pickedUpItem = 'kubejs:obsidian_lava_bucket'
+        sound = 'minecraft:item.bucket.fill_lava'
+    } else if (block.id == 'minecraft:powder_snow') {
+        pickedUpItem = 'kubejs:obsidian_powder_snow_bucket'
+        sound = 'minecraft:item.bucket.fill_powder_snow'
+    }
+
+    if (pickedUpItem) {
+        block.set('minecraft:air')
+        player.swing()
+        
+        event.server.runCommandSilent(`/playsound ${sound} block @p ${player.x} ${player.y} ${player.z} 1 1`)
+        
+        player.give(pickedUpItem)
+
+        if (!player.creative) {
+            item.shrink(1)
         }
     }
 })
