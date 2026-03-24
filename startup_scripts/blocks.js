d392285c515690fde02be855571c84f4212b2ad7
bbitsme <hascallblake71@gmail.com>
2026-03-24 12:22:49 -0400

    .

--- startup_scripts/blocks.js
+++ startup_scripts/blocks.js
@@ -1,15 +1,59 @@
 StartupEvents.registry('block', event => {
-  event.create('charcoal_block')
-    .displayName('Block of Charcoal')
-    .soundType('stone')
-    .hardness(5)
-    .resistance(6)
-    .tagBlock('minecraft:mineable/pickaxe')
+    event.create('charcoal_block').displayName('Block of Charcoal').soundType('stone')
+        .hardness(5)
+        .resistance(6)
+        .tagBlock('minecraft:mineable/pickaxe')
     
-  event.create('cracked_diamond_block')
-    .displayName('Cracked Diamond Block')
-    .soundType('stone')
-    .tagBlock('minecraft:mineable/pickaxe')
-    .hardness(5)
-    .resistance(6)
+    event.create('cracked_diamond_block').displayName('Cracked Diamond Block').soundType('stone')
+        .tagBlock('minecraft:mineable/pickaxe')
+        .hardness(5)
+        .resistance(6)
+
+    event.create('budding_durite').displayName('Budding Durite').soundType('amethyst')
+        .tagBlock('minecraft:mineable/pickaxe')
+        .randomTick(tickEvent => {
+          const { block, level } = tickEvent
+          
+          if (Math.random() < 0.2) {
+              const directions = [Direction.UP, Direction.DOWN, Direction.NORTH, Direction.SOUTH, Direction.EAST, Direction.WEST]
+              let side = directions[Math.floor(Math.random() * directions.length)]
+              
+              let targetPos = block.pos.relative(side)
+              let targetBlock = level.getBlock(targetPos)
+              
+              if (targetBlock.id == 'minecraft:air') {
+                  targetBlock.set(`dungeonnowloading:small_durite_bud`, {facing: side})
+              } else if (targetBlock.id == 'dungeonnowloading:small_durite_bud') {
+                  targetBlock.set(`dungeonnowloading:medium_durite_bud`, {facing: side})
+              } else if (targetBlock.id == 'dungeonnowloading:medium_durite_bud') {
+                  targetBlock.set(`dungeonnowloading:large_durite_bud`, {facing: side})
+              } else if (targetBlock.id == 'dungeonnowloading:large_durite_bud') {
+                  targetBlock.set(`dungeonnowloading:durite_cluster`, {facing: side})
+              }
+          }
+      })
+
+    event.create('cursed_budding_durite').displayName('Cursed Budding Durite').soundType('amethyst')
+        .tagBlock('minecraft:mineable/pickaxe')
+        .randomTick(tickEvent => {
+          const { block, level } = tickEvent
+          
+          if (Math.random() < 0.2) {
+              const directions = [Direction.UP, Direction.DOWN, Direction.NORTH, Direction.SOUTH, Direction.EAST, Direction.WEST]
+              let side = directions[Math.floor(Math.random() * directions.length)]
+              
+              let targetPos = block.pos.relative(side)
+              let targetBlock = level.getBlock(targetPos)
+              
+              if (targetBlock.id == 'minecraft:air') {
+                  targetBlock.set(`dungeonnowloading:small_durite_bud`)
+              } else if (targetBlock.id == 'dungeonnowloading:small_durite_bud') {
+                  targetBlock.set(`dungeonnowloading:medium_durite_bud`)
+              } else if (targetBlock.id == 'dungeonnowloading:medium_durite_bud') {
+                  targetBlock.set(`dungeonnowloading:large_durite_bud`)
+              } else if (targetBlock.id == 'dungeonnowloading:large_durite_bud') {
+                  targetBlock.set(`dungeonnowloading:durite_cluster`)
+              }
+          }
+      })
 })
