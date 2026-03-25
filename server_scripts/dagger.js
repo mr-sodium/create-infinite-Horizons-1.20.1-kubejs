616e13fbc7f453665dd51d0548f83d9005162f32
mr-sodium <jk8467445@gmail.com>
2026-03-25 00:20:13 -0400

    were so cooked. also borken

--- server_scripts/dagger.js
+++ server_scripts/dagger.js
@@ -1,6 +1,7 @@
 ItemEvents.rightClicked('kubejs:dagger', event => {
-    const { item, player, hand, server } = event
-    
+    const { item, player, hand, server, level } = event
+    const reach = 4.5
+    const result = player.rayTrace(reach, true)
     if (hand != 'MAIN_HAND') return
 
     if (!player.creative) {
@@ -11,6 +12,12 @@ ItemEvents.rightClicked('kubejs:dagger', event => {
         if (item.damageValue >= item.maxDamage) {
             item.count--
             server.runCommandSilent(`/playsound minecraft:entity.item.break block @p ${player.x} ${player.y} ${player.z} 1 1`)
+        }
+    }
+    console.log(result.block.x+1)
+    if(result.block == 'minecraft:red_concrete'){   //red concrete is to be replaced with spell funnel
+        if(level.getBlock(result.block.x + 1, result.block.y, result.block.z).id == 'minecraft:yellow_concrete' && level.getBlock(result.block.x - 1, result.block.y, result.block.z).id == 'minecraft:yellow_concrete' && level.getBlock(result.block.x, result.block.y, result.block.z + 1).id == 'minecraft:yellow_concrete' && level.getBlock(result.block.x, result.block.y, result.block.z - 1).id == 'minecraft:yellow_concrete' ){
+            console.log("testdone")
         }
     }
 })
