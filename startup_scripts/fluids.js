69770f2f961cbfe92d4f686b9edb3b6a1a770e62
bbitsme <hascallblake71@gmail.com>
2026-04-15 18:45:20 -0400

    Added peppi stuffs

--- startup_scripts/fluids.js
+++ startup_scripts/fluids.js
@@ -1,3 +1,4 @@
 StartupEvents.registry('fluid', event => {
       event.create('raw_rubber').thinTexture(0xEDE7D0).bucketColor(0xEDE7D0).displayName('Raw Rubber').density(3000).viscosity(8000).noBlock()
+      event.create('peppi').thinTexture(0x89ff00).bucketColor(0x89ff00).displayName('Peppi').noBlock().bucket
 })
